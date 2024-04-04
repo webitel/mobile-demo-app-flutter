91883f3d8c9b2df6a1de5ed46af3d01576a4d400
VolodiaHunkalo <graperz123@gmail.com>
2024-04-04 22:33:55 +0300

    chat ui

--- lib/presentation/page/chat.dart
+++ lib/presentation/page/chat.dart
@@ -1,70 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+  const ChatPage({
+    super.key,
+  });
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  String initialMessage = 'Test';
+  final TextEditingController _textEditingController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: const Text(
-          'Chat',
-          style: TextStyle(
-              fontSize: 24, color: Colors.white, fontWeight: FontWeight.w600),
-        ),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 700,
-                child: ListView.builder(
-                  itemCount: 2,
-                  itemBuilder: (BuildContext context, message) {
-                    return const SizedBox();
-                  },
-                ),
-              ),
-              Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 6),
-                    height: 60,
-                    width: 280,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.grey.withOpacity(0.5),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 9,
+            child: ListView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.83,
+                  child: Scrollbar(
+                    controller: _scrollController,
+                    trackVisibility: true,
+                    thickness: 2,
+                    thumbVisibility: true,
+                    child: ListView.builder(
+                      itemCount: 20,
+                      itemBuilder: (BuildContext context, message) {
+                        return Container(
+                          margin: const EdgeInsets.only(
+                              top: 8, bottom: 8, left: 200, right: 20),
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.yellow.withOpacity(0.2),
+                          ),
+                          child: const Center(
+                              child: Text(
+                            'Test message',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )),
+                        );
+                      },
                     ),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Write your message',
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Divider(thickness: 2),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              constraints: const BoxConstraints(maxHeight: 127),
+                              child: TextField(
+                                textCapitalization:
+                                    TextCapitalization.sentences,
+                                maxLines: null,
+                                keyboardType: TextInputType.multiline,
+                                controller: _textEditingController,
+                                onChanged: (value) {},
+                                decoration: const InputDecoration(
+                                  hintText: 'Type here',
+                                  border: InputBorder.none,
+                                  enabledBorder: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 16),
+                          GestureDetector(
+                            onTap: () {},
+                            child: SizedBox(
+                              height: 40,
+                              width: 40,
+                              child: SvgPicture.asset(
+                                'assets/icons/send_message.svg',
+                                colorFilter: const ColorFilter.mode(
+                                    Colors.green, BlendMode.srcIn),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Send"),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
